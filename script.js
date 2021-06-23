ee439acc90b817b1afbc7d5a0494acee3ade4497
nishthaneeraj <nishthachess64@gmail.com>
2021-06-23 21:37:23 +0530

    hello

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-current_year = new Date().getFullYear();
-
-// returns the current year
-document.getElementById("year").innerHTML = current_year;
-
+// current_year = new Date().getFullYear();
+//
+// // returns the current year
+// // document.getElementById("year").innerHTML = current_year;
